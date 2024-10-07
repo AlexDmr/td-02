836eefecfbe22fb5eaea6f53ff036e8f1f4e16ad
alexandre.demeure <alexandre.demeure@univ-grenoble-alpes.fr>
2024-10-07 15:38:49 +0200

    ++

--- src/td05/Analyseur.java
+++ src/td05/Analyseur.java
@@ -7,36 +7,7 @@ public class Analyseur {
      * @return the integer value of the string s, evaluated as an arithmetic expression
      */
     int eval(String s) {
-        Fragment[] fragments = parse(s);
-        Pile pile = new Pile();
-        for (Fragment fragment : fragments) {
-            if (fragment.type == Type.num) {
-                pile.empiler(fragment);
-            } else {
-                Fragment f2 = pile.dépiler();
-                Fragment f1 = pile.dépiler();
-                int n1 = entier(f1.value);
-                int n2 = entier(f2.value);
-                int result = 0;
-                switch (fragment.value) {
-                    case "+":
-                        result = n1 + n2;
-                        break;
-                    case "-":
-                        result = n1 - n2;
-                        break;
-                    case "*":
-                    case "x":
-                        result = n1 * n2;
-                        break;
-                    case "/":
-                        result = n1 / n2;
-                        break;
-                }
-                pile.empiler(new Fragment(Type.num, Integer.toString(result)));
-            }
-        }
-        return entier(pile.dépiler().value);
+        return 0;
     }
 
     /**
