0bfee1f9c60fce558975e8a261be03fe811c0d8b
alexandre.demeure <alexandre.demeure@univ-grenoble-alpes.fr>
2024-10-07 17:03:31 +0200

    Voir le dernier teste de testons.java, il reste une correction à apporter dans le code.

--- src/td05/Analyseur.java
+++ src/td05/Analyseur.java
@@ -1,26 +1,65 @@
 package td05;
 
+import java.util.Optional;
+
 public class Analyseur {
     /**
      * à faire
      * @param s string to evaluate, containing an arithmetic expression formatted in inverse Polish notation.
      * @return the integer value of the string s, evaluated as an arithmetic expression
      */
-    int eval(String s) {
+    Optional<Integer> eval(String s) { // Optional est une classe générique, elle ne s'applique que sur des classes, d'où l'utilisation de Integer plutot que int
         Fragment[] L = parse(s);
         Pile P = new Pile();
 
-        /**
-         * ça donne une idée de solution MAIS il faudrait placer ça dans 
-         * une boucle englobante pour pouvoir tout parcourir
-         */
-        int i = 0;
-        while (i != L.length && L[i].type != Type.op) {
-            P.empiler( L[i] );
-            i++;
-        }
+        for (Fragment f : L) {
+            if (f.type == Type.num) {
+                P.empiler(f);
+            } else {
+                Optional<Integer> B = entier(P.dépiler().value);
+                Optional<Integer> A = entier(P.dépiler().value);
+                Optional<Integer> res;
 
-        return 0;
+                if (A.isEmpty() || B.isEmpty()) {
+                    return Optional.empty(); // Attention ça n'est pas conseillé dans ce cours de placer des returns dans les for
+                } else {
+                    int a = A.get();
+                    int b = B.get();
+
+                    switch (f.value) {
+                        case "+":
+                            res = Optional.of(a + b);
+                            break;
+                        case "-":
+                            res = Optional.of(a - b);
+                            break;
+                        case "*":
+                        case "x":
+                            res = Optional.of(a * b);
+                            break;
+                        case "/":
+                            // On évite ici la cas indéfinit de la division par 0.
+                            res = b == 0 ? Optional.empty() : Optional.of(a / b);
+                            break;
+                        default:
+                            // Opérateur inconnu --> L'expression n'est pas définie
+                            res = Optional.empty();
+                    }
+
+                    if (res.isEmpty())
+                        return Optional.empty(); // Même remarque sur le return dans le for...
+                    
+                    // Si on est ici, c'est que res est porteur d'une valeur.
+                    P.empiler(
+                        new Fragment(Type.num, Integer.toString(res.get()))
+                    );
+                }
+            }
+        }
+        
+        return entier(
+            P.dépiler().value
+        );
     }
 
     /**
@@ -41,8 +80,14 @@ public class Analyseur {
     }
 
     // Return the integer value of the string s
-    int entier(String s) {
-        return Integer.parseInt(s);
+    Optional<Integer> entier(String s) { // Optional est une classe générique, elle ne s'applique que sur des classes, d'où l'utilisation de Integer plutot que int
+        // On traite l'exception
+        // Le mieux dans notre approche aurait été que Integer.parseInt renvoie un Optional<Integer>
+        try {
+            return Optional.of( Integer.parseInt(s) );
+        } catch(NumberFormatException e) {
+            return Optional.empty();
+        }
     }
 
 }
--- src/td05/essai.java
+++ src/td05/essai.java
@@ -4,6 +4,6 @@ public class essai {
     public static void main(String[] args) {
         // Un essai avec une expression simple "1 2 +"
         Analyseur A = new Analyseur();
-        A.eval("1 2 +");
+        System.out.println( A.eval("0 0 /") );
     }
 }
